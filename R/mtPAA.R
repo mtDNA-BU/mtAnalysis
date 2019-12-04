c59fd8234a1dc85bfdae3a658a65d29bea59370d
Katerina Oleinik <koleinik@bu.edu>
2019-12-04 09:45:59 -0500

    Updated the description of mtPAA function and removed coverage and loci input parameters

--- R/mtPAA.R
+++ R/mtPAA.R
@@ -1,25 +1,19 @@
 #' mtPAA function
 #'
-#' Calculate the Proportion of Alternative Allele of Each Locus for Each Individual
+#' Derivation of alternative allele fraction
 #'
-#' @param coverage Coverage, numeric matrix
 #' @param allele Allele dataset, numeric matrix
 #' @param freq Frequency dataset, numeric matrix
-#' @param loci Numeric vector, default 1:16569
 #'
 #' @return PAA, numeric matrix with dimensions of n subjects x 16569 SNPs
 #' @export
 #' @examples
 #'
 #' #Read input data
-#' coverage_file <- "/restricted/projectnb/mtdna-alcohol/Sun_Xianbang/ARIC/coverage/coverage.csv"
 #' allele_file <- "/restricted/projectnb/mtdna-alcohol/Sun_Xianbang/ARIC/allele/allele.csv"
 #' freq_file <- "/restricted/projectnb/mtdna-alcohol/Sun_Xianbang/ARIC/freq/freq.csv"
 #'
 #' #head <- scan( file = allele_file, sep = ",", character(), nlines = 1, quiet = TRUE)
-#' #coverage <- matrix( scan( file = coverage_file,
-#' #                          sep = ",", character() ),
-#' #                    ncol = length( head ), byrow = TRUE)
 #' #allele   <- matrix( scan( file = allele_file,
 #' #                          sep = ",", character() ),
 #' #                    ncol = length( head ), byrow = TRUE)
@@ -28,37 +22,26 @@
 #' #                    ncol = length( head ), byrow = TRUE)
 #'
 #' #mtPAA (coverage, allele, freq)
-mtPAA <- function( coverage, allele, freq, loci = 1:16569 ){
+mtPAA <- function( allele, freq ){
 
 
-  if((sum(dim(coverage) != dim(allele) ) > 0) | (sum(dim(coverage) != dim(freq))>0)){
-    stop("the coverage, allele and frequency should have same dimension")
-  }
+  # if((sum(dim(coverage) != dim(allele) ) > 0) | (sum(dim(coverage) != dim(freq))>0)){
+  #   stop("the coverage, allele and frequency should have same dimension")
+  # }
 
   # order the allele, freq and coverage datasets by ID
   allele <- allele[order(allele[,1]), ]
   freq <- freq[order(freq[,1]), ]
-  coverage <- coverage[order(coverage[,1]), ]
 
   # record the IDs in the vector of subjectID, and remove the ID column from the original dataset
   subjectID <- allele[,1]
   allele <- allele[,-1]
   freq   <- freq[,-1]
-  coverage<- coverage[,-1]
 
   # transpose the allele, freq and coverage datasets, so each column vector represents the sequence of a subject
   allele <- t(allele)
   freq <- t(freq)
-  coverage <- t(coverage )
 
-
-  if (length(loci)!=(dim(allele)[1])){
-    stop("the length of loci should be the same as the dataset")
-  }
-
-  if(length(loci)>16569){
-    warning("loci should have no more than 16569")
-  }
 
   AAF3.m <- array(0, dim(allele) )
   complex.allele <- which(allele != .mtRef)
