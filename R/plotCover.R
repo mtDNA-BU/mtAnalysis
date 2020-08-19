5d8fee0a19d47ee5ad8427e1a2b14fc4760a94ae
Xianbang Sun <sxb3000@bu.edu>
2020-08-19 12:14:10 -0400

    modify main title of the histogram

--- R/plotCover.R
+++ R/plotCover.R
@@ -32,7 +32,7 @@
 #'
 plotCover <- function(x, loci=seq_len(.mtLength), col="blue", pch='.', cex=0.2,
                       xlab="mtloci",
-                      ylab="", main="Mean Coverage across all mtDNA loci",
+                      ylab="", main="Mean Coverage for all mtDNA loci",
                       ...) {
 
     if(dim(x)[1] != .mtLength)
